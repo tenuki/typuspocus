6839d86558c04fb75ebe342382fdd4e969f3c39a
lucio.torre <lucio.torre>
2006-09-03 16:16:16 +0000

    sonidos!

---
+++ sounds.py
@@ -0,0 +1,7 @@
+import pygame
+
+reloj = pygame.mixer.Sound("sounds/Reloj.ogg")
+bravo = pygame.mixer.Sound("sounds/GenteContenta.ogg")
+bravo2 = pygame.mixer.Sound("sounds/Felicitaciones01.ogg")
+abucheo = pygame.mixer.Sound("sounds/Abucheo01.ogg")
+abucheo2 = pygame.mixer.Sound("sounds/Abucheo03.ogg")
