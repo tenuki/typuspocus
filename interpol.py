f5f179d101ea43bb97d7d9782e81bf48d2693f7f
tenuki <tenuki>
2006-09-03 20:31:00 +0000

    mejor interpolacion.. aunque solo usamos rectas..

--- interpol.py
+++ interpol.py
@@ -9,18 +9,23 @@ class MadamBezier(PInterpolator):
     def __init__(self, pinicio, pfin, prev=None):
         PInterpolator.__init__(self,pinicio, pfin)
         d = self.fin-self.inicio
-        if prev<>None:
-            self.m1 = 2*self.inicio - prev
+        if 0: #prev<>None:
+            self.m1 = 2*self.inicio - prev.getPrev()
         else:
             self.m1 = self.inicio + d/4    
         self.m2 = self.fin-d/4 #random here please 
+        print self.inicio
+        print self.m1
+        print self.m2
+        print self.fin
         
     def getAt(self, t):
-        t=1-t
+        #t=1-t
         c = ((1-t)**3)*self.inicio + \
                 3*t*((1-t)**2)*self.m1+\
                 3*(t**2)*(1-t)*self.m2+\
                 (t**3)*self.fin 
+        print c.real, c.imag
         return c.real, c.imag
         
     def getPrev(self):
--- varitaje.py
+++ varitaje.py
@@ -6,37 +6,35 @@ boundsRect = pygame.Rect(0,200,700,400)
 
 class Varitaje:
     def __init__(self):
-        self.pos = (400,450)
         self.l = {False:((000,300),(300,600)),
                     True: ((300,300),(700,600)) }
         self.last = True
         self.step = 0
-        self.max = 30
+        self.max = 15
         self.prev = None
         self.inter = None
         self.points = []
+        self.generatePoint()
+        self.pos = 400,400
         
     def generatePoint(self):
         where = self.l[self.last]
         self.last = not self.last
         inicio, fin = where
         rand = random.randint(inicio[0],fin[0]),random.randint(inicio[1],fin[1])
-        self.points.append(rand)
+        self.points.append( rand  )
         
     def nextpos(self):
-        r=pygame.Rect( pygame.mouse.get_pos(), (1,1) )
-        return r.clamp(boundsRect).center
-        if len(self.points)<5: 
-            for i in range(5): 
-                self.generatePoint()
+        if len(self.points)<=0: 
+            self.generatePoint()
         if self.step%self.max==0:
+            print '-'*10
             self.step=0
             self.inter = interpol.MadamBezier( 
                             self.pos, self.points.pop(0), self.inter )
         self.step+=1
-        interpos = self.inter.getAt( 3.03 * (self.step-1) )
-        print interpos
-        return interpos
+        self.pos = self.inter.getAt( (1.0/self.max) * (self.step-1) )
+        return self.pos
 
 if __name__ == "__main__":
     pass
