88a5a1d024cc7e925566b6cd649ef215fd4cb365
ricardoquesada <ricardoquesada>
2006-09-06 04:23:34 +0000

    server for the hiscores

---
+++ hiscore_server.py
@@ -0,0 +1,188 @@
+#
+# A simple http server for saving hi scores
+# pyweek - PyAr2
+#
+
+import SimpleHTTPServer
+import BaseHTTPServer
+import time
+import os
+import pickle
+
+class Singleton:
+    __single = None
+    def __init__( self ):
+        if Singleton.__single:
+            raise Singleton.__single
+        Singleton.__single = self    
+
+class HiScoreData( Singleton ):
+
+    POINTS = 0
+    COMMIT_TIME = 1         # added for comparison
+    NAME = 2
+    WHEN = 3
+    IPADDR = 4
+    ASCII_TIME = 5
+   
+    HISCORE_FILENAME = 'typos_pocus_hi_scores.pickle'
+    
+    def __init__( self ):
+        Singleton.__init__( self )
+        self.hiScores = []
+        self.loadHiScores()
+        self.save_counter = 0
+
+    def loadHiScores(self):
+        try:
+            os.stat( HiScoreData.HISCORE_FILENAME)
+            f = file(HiScoreData.HISCORE_FILENAME)
+            self.hiScores = pickle.load( f )
+            f.close()
+        except OSError:
+            pass
+
+    def saveHiScores(self):
+        print 'saveHiScores()'
+        try:
+            f = file(HiScoreData.HISCORE_FILENAME,'w+')
+            pickle.dump(self.hiScores, f)
+            f.close()
+        except Exception, e:
+            print 'Coult not save hi scores'
+            print e
+
+    def addHiScore( self, points, name, ipaddr):
+        print 'addHiScore(%s,%s,%s)' % ( points, name, ipaddr )
+        real_when = time.time()
+        ascii_when = time.ctime()
+        self.hiScores.append( (int(points),real_when,name,ipaddr,ascii_when) )
+        self.hiScores.sort()
+        self.hiScores.reverse()
+
+        self.save_counter += 1
+        if self.save_counter % 1 == 0:
+            self.saveHiScores()
+
+    def listHiScores( self ):
+        # only return the 50 first records
+        return self.hiScores[:50]
+
+    def __del__(self):
+        self.saveHiSCores()
+
+def Handle( x = Singleton ):
+    try:
+        single = x()
+    except Singleton, s:
+        single = s
+    return single    
+
+
+class HiScoresHandler( SimpleHTTPServer.SimpleHTTPRequestHandler ):
+
+    def hs_listHiScores(self):
+
+        self.wfile.write('<!DOCTYPE html PUBLIC "-//w3c//dtd html 4.0 transitional//en">\n')
+        self.wfile.write("<html>\n<head>\n<title>Typos Pocus - Hi Scores</title>\n</head>\n")
+        linestr ="<body>\n"
+        data = Handle(HiScoreData)
+
+        linestr +='<center>'
+        linestr +='<table BORDER=0 BGCOLOR="#dedede" NOSAVE>\n'
+        linestr +="<th><center>Hi Scores</center></th>"
+        linestr +="<tr>\n"
+        linestr +="\t<td><b>Points</b></td>\n"
+        linestr +="\t<td><b>Name</b></td>\n"
+        linestr +="\t<td><b>IP Address</b></td>\n"
+        linestr +="\t<td><b>When</b></td>\n"
+        linestr +="</tr>\n"
+
+        index = 0
+        for i in data.listHiScores():
+            if index % 2 == 0:
+                linestr +='<tr BGCOLOR="#eeeeee" NOSAVE>\n'
+            else:
+                linestr +='<tr BGCOLOR="#dedede" NOSAVE>\n'
+
+            index += 1
+
+            linestr +="<!-- entry -->"
+            try:
+                for j in range(0,len(i)):
+                    if j % len(i) == HiScoreData.COMMIT_TIME:
+                        continue
+                    linestr +="<td>%s</td>" % i[j]
+                linestr +="</tr>\n"
+            except Exception, e:
+                print str(e)
+                print 'Invalid entry:'
+                print i
+
+        linestr +="</table>\n"
+        linestr +='</center>\n'
+        linestr +="</body>\n</html>\n"
+        self.wfile.write('\n' + linestr)
+
+    def hs_listHiScores_xml(self):
+
+        data = Handle(HiScoreData)
+        linestr = '<?xml version="1.0"?>\n'
+        linestr += '<hiscores version="1.0">\n'
+
+        for i in data.listHiScores():
+            linestr += '\t<entry>\n'
+            try:
+                len_i = len(i)
+                for j in range(0,len_i):
+                    if j % len_i == 0:
+                        linestr +="\t\t<points>%d</points>\n" % i[j]
+                    if j % len_i == HiScoreData.COMMIT_TIME:
+                        continue
+                    if j % len_i == 2:
+                        linestr +="\t\t<name>%s</name>\n" % i[j]
+                    if j % len_i == 3:
+                        linestr +="\t\t<when>%s</when>\n" % i[j]
+                    if j % len_i == 4:
+                        linestr +="\t\t<ipaddress>%s</ipaddress>\n" % i[j]
+            except:
+                print 'Invalid entry:'
+                print i
+            linestr +="\t</entry>\n"
+        linestr +='</hiscores>\n'
+        self.wfile.write(linestr)
+
+    def hs_addHiScore(self):
+        if len(self.hiscore_params) == 2:
+            data = Handle(HiScoreData)
+            data.addHiScore( self.hiscore_params[0], self.hiscore_params[1], self.client_address[0])
+            self.wfile.write('<html><head><title>Hi Scores Added</title></head></html>')
+        else:            
+            self.send_error(400, "invalid parameters")
+
+    def do_GET(self):
+        """Serve a GET request."""
+
+        self.hiscore_command = self.path[1:].split('/')  
+
+        if len(self.hiscore_command) > 0:
+                mname = 'hs_' + self.hiscore_command[0]
+                if not hasattr(self, mname):
+                    self.send_error(501, "Unsupported method (%s)" % `self.hiscore_command[0]`)
+                    return
+                self.hiscore_params = self.hiscore_command[1:]
+                method = getattr(self, mname)
+                method()
+        else:
+            self.send_error(400, "Invalid request" )
+            return
+
+    def do_HEAD(self):
+        pass
+
+def main(HandlerClass = HiScoresHandler, ServerClass = BaseHTTPServer.HTTPServer):
+    BaseHTTPServer.test(HandlerClass, ServerClass)
+
+
+if __name__ == "__main__":
+    main()
