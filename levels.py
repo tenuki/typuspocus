20f1dc6d95d9281b1770b0bc8a0e6f40a752b3f5
tenuki <tenuki>
2006-09-09 18:43:21 +0000

    intro de niveles..

--- levels.py
+++ levels.py
@@ -94,8 +94,8 @@ visitor's MVP lucky sneakers.""",
         "Hip-hoperos o darkies",
         "Alcohol, drogas, hacer un mix",
         "sexus drugus rockanrolus, captivus crowdimus yowasaaap. cops and hardcopy marihuanus boozelion, cocuchus chuchu fuchu.",
-"""You were supposed to dissapear a 
-milli vanilli cd, 
+"""You were supposed to dissapear 
+a milli vanilli cd, 
 instead you must vanish the 
 drugs to hide them 
 from the cops.""",
@@ -111,7 +111,11 @@ from the cops.""",
         "Gente adinerada",
         "papeles y documentos",
         "factus taxus nuLus, nilun actin, evaDus fiscus IRSus, whySaw Elvis Bellagium 11 ocean's cardus impustum disapiros ipsofactum.",
-        "You get to Las Vegas to perform in a Casino, but the owners want to take advantage of your power to evade taxes.",
+"""You get to Las Vegas 
+to perform in a Casino, but 
+the owners want to take 
+advantage of your power 
+to evade taxes.""",
         "The IRS wants you for beeing an accesory to tax evasion. They send you Africa where they'll never find you",
         "Your act was a total failure, you got wasted and lost all your money playing craps.",
         "Mumm-Ra",
@@ -124,7 +128,11 @@ from the cops.""",
         "Africanitos de la tribu",
         "Un muricélago, o algún animal raro",
         "Batsimus chicaka aFrIcuN triBuson, ill chikakun medicor, nigrum. comedo. crudus...pain cooking Shutlewor meellon cHuNgo.",
-        "You go to live with a tribe in africa,  they find out you are a wizard and want you to heal their ill sacred animal",
+"""You go to live with a tribe 
+in africa, they find out
+you are a wizard and want 
+you to heal their ill 
+sacred animal""",
         "You dissapeared the sacred animal. The angry natives start chasing you around the jungle.",
         "The natives made a tasty casserole out of you.",
         "Harry Potter",
@@ -136,7 +144,10 @@ from the cops.""",
         "Robots de star wars",
         "Aspiradora loca",
         "Arturitum Vacuumcleanerum Ev4niscum c3p0 venusiun rescusum grossum ph1ll1ps, disapirum spacious robotitus shakulus.",
-        "While running away from the natives, a spaceship abdu^H^H^H^Hrescues you. The venusians ask you to help them vanish a hitchhiker.",
+"""While running away from the natives, 
+a spaceship abdu^H^H^H^Hrescues 
+you. The venusians ask you to help 
+them vanish a hitchhiker.""",
         "Since you were so helpfull, the venusians send you back to earth in an individual spaceship.",
         "Because you were unwilling to cooperate, the venusians performed a 'light saber anal probing' on you.",
         "Mandrake",
@@ -148,7 +159,11 @@ from the cops.""",
         "Marcianitos y men in black",
         "un marcianito de roswell",
         "Marcianus 51area alf rosswellin didosong untilyourestingherewithme evanisum marcianus y guarda que viene molderrr.",
-        "Your ship was running a pirated OS and crashed in new mexico. The FBI captures you and asks you to dissapear some evidence of the alien landings.",
+"""Your ship was running a pirated 
+OS and crashed in new mexico. 
+The FBI captures you and asks you 
+to dissapear some evidence of 
+the alien landings.""",
         "Now that the evidence exists only on your memory, the FBI makes sure you won't talk by putting a cap in your head",
         "You couldn't do it. But you'll have a lot of time to practice since you are never leaving area 51",
         "Harry Houdini",
@@ -160,7 +175,11 @@ from the cops.""",
         "Esqueletos",
         "Estatuilla de Anubis",
         "vaderetrum satinus anubisun chungus, transformix this decayed form to Mumm-ra the Everliving and the puwur of christus will savius vox.",
-        "Ok, you died, and went to hell, you can escape to heaven by dissapearing Anubis. All the dead gather around to watch...",
+"""Ok, you died, and went to hell, 
+you can escape to heaven by 
+dissapearing Anubis. 
+
+All the dead gather around to watch...""",
         "You did it!!! Now you go to heaven, that happy place we all dream about as kids, that place where men and women love each other. ",
         "You stay in hell. At least you won't have problems finding a lawyer if you ever come to need one.",
         "Merlyn",
@@ -173,7 +192,10 @@ from the cops.""",
         "Todos desnudos",
         "CUZCO la cabra",
         "caelum CUZCUS 13 paradisiun, revivisco animalis playboyus housus, pornus sexus gross klunx workus my clunk essta noshi inbolus minusem. miau miau.",
-        "Here we are, it's beautiful, everything that dissapears here goes back to earth, so you are going to revive a lovely goat now!",
+"""Here we are, it's beautiful, 
+everything that dissapears here goes 
+back to earth, so you are going to 
+revive a lovely goat now!""",
         "Congratulations! You fullfilled your destiny as a magician!",
         "Mmm... Too bad you couldn't do it, but hey, a horde of naked angels offers to console you!!",
         "Gandalf",
